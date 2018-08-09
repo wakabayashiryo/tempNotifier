308140458c021a1d6958ed997bd01cd20c33bf68
EVAOTA <nervevawaka@ezweb.ne.jp>
2018-08-09 12:08:31 +0900

    using IFTT,Successed notify over LINE

--- bmp280test.ino
+++ bmp280test.ino
@@ -1,51 +1,96 @@
-/***************************************************************************
-  This is a library for the BMP280 humidity, temperature & pressure sensor
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
 
-  Designed specifically to work with the Adafruit BMEP280 Breakout 
-  ----> http://www.adafruit.com/products/2651
+// 転送レート
+#define SERIAL_SPEED  115200
 
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required 
-  to interface.
+// Wi-Fi SSID
+#define WLAN_SSID         "aterm-912afc-g"
+// Wi-Fi パスワード
+#define WLAN_PASS         "39f9398943819"
 
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
+// IFTTTのホスト名
+#define IFTTT_HOST_NAME  "maker.ifttt.com"
 
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
+// IFTTTのイベント名
+#define IFTTT_EVENT_NAME  "temp_notify"
 
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
+// IFTTTのシークレットキー
+#define IFTTT_KEY         "cdjsD1Qw-5TOT4G3t53Zv_"
 
+// ポート番号
+#define PORT_NUMBER       80
 
-Adafruit_BMP280 bme; // I2C
-  
 void setup() {
-  Serial.begin(9600);
-  Serial.println(F("BMP280 test"));
-  Wire.begin(4,5);
-  if (!bme.begin()) {  
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    while (1);
-  }
-}
-  
-void loop() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print("Pressure = ");
-    Serial.print(bme.readPressure());
-    Serial.println(" Pa");
+  Serial.begin(SERIAL_SPEED);
+  delay(10);
 
-    Serial.print("Approx altitude = ");
-    Serial.print(bme.readAltitude(1013.25)); // this should be adjusted to your local forcase
-    Serial.println(" m");
-    
-    Serial.println();
-    delay(2000);
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() 
+{
+   StaticJsonBuffer<200> jsonbuff;
+  JsonObject& dat = jsonbuff.createObject();
+
+  dat["value1"] = "日本語";
+  
+  Serial.print("connecting to ");
+  Serial.println(IFTTT_HOST_NAME);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;  
+  if (!client.connect(IFTTT_HOST_NAME, PORT_NUMBER)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // IFTTTへ送信するデータ
+
+  char sData[200];
+  String value;
+  dat.printTo(value);
+  value += "\r\n";
+  int contentlength = value.length();
+  
+  sprintf(sData,"POST http://maker.ifttt.com/trigger/%s/with/key/%s/ HTTP/1.1\r\nHost:maker.ifttt.com\r\nContent-Length:%d\r\nContent-Type: application/json\r\n\r\n%s\r\n",IFTTT_EVENT_NAME,IFTTT_KEY,contentlength,value.c_str());
+  Serial.print(sData);
+  // This will send the request to the server
+  client.print(sData);
+  int timeout = millis() + 5000;
+  while (client.available() == 0) {
+    if (timeout - millis() < 0) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+
+  delay(5000);
 }
